01b00762508069d9a57db0453b9042c420ca0bfe
Felipe Santos <felipe.santos@qualyteam.com.br>
2025-03-04 19:19:10 -0300

    Add: Keen Slider to make Carousel in Products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
-import { HomeContainer, Product } from "@/styles/pages/home";
 import Image from "next/image";
+import { useKeenSlider } from "keen-slider/react";
+import 'keen-slider/keen-slider.min.css';
+
+import { HomeContainer, Product } from "@/styles/pages/home";
 
 import camiseta1 from '../assets/shirts/1.png'
 import camiseta2 from '../assets/shirts/2.png'
 import camiseta3 from '../assets/shirts/3.png'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
 
         <footer>
@@ -17,11 +27,29 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Z</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta A</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
