c8418786810384e49ae757097baa8f134859f97d
Felipe Santos <felipe.santos@qualyteam.com.br>
2025-03-09 18:01:09 -0300

    Add: Product Page Styles

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,23 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
 import { useRouter } from "next/router"
 
 export default function Product() {
     const { query } = useRouter();
 
     return (
-        <h1>Product: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+                
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quia, nostrum similique consequuntur voluptas dolorem, quae perspiciatis ullam aliquid numquam fugiat. Ipsam, quia temporibus porro voluptatibus nesciunt error incidunt excepturi!</p>
+                
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
---
+++ src/styles/pages/product.ts
@@ -0,0 +1,72 @@
+import { styled } from "..";
+
+export const ProductContainer = styled('main', {
+    maxWidth: '1180px',
+
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    alignItems: 'stretch',
+    gap: '4rem',
+    margin: '0 auto',
+});
+
+export const ImageContainer = styled('div', {
+    width: '100%',
+    maxWidth: '576px',
+    height: '656px',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '0.25rem',
+
+    background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+    borderRadius: '8px',
+
+    image: {
+        objectFit: 'cover',
+    }
+});
+
+export const ProductDetails = styled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+
+    h1: {
+        fontSize: '$2xl',
+        color: '$gray300',
+    },
+
+    span: {
+        display: 'block',
+        marginTop: '1rem',
+        fontSize: '$2xl',
+        color: '$green300',
+    },
+
+    p: {
+        marginTop: '2.5rem',
+        fontSize: '$md',
+        color: '$gray300',
+        lineHeight: '1.6',
+    },
+
+    button: {
+        marginTop: 'auto',
+        padding: '1.25rem',
+
+        fontSize: '$md',
+        fontWeight: 'bold', 
+        color: '$white',
+        backgroundColor: '$green500',
+
+        border: 'none',
+        borderRadius: '8px',
+
+        cursor: 'pointer',
+
+        '&:hover': {
+            backgroundColor: '$green300',
+        }
+    }
+});
